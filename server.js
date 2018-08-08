ea56bc5f2bb9b742cdb408c4398a286165c54dae
Chris Burgdorff <wesborland1234@aol.com>
2018-08-07 21:06:54 -0400

    worked on layout

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var app = express();
 //Variables
 var port = process.env.PORT || 3000;
 //Routes
+/*
 app.get('/', function (req, res){
   res.send("HELLO THERE!");
-});
+});  */
+app.use(express.static(__dirname + "/public"));
 //Start Server
 app.listen(port, function() {
-  console.log("App is NOT FUCKING LISTENING BRO listening on port 3000.")
+  console.log("App is listening on port 3000.")
 });
