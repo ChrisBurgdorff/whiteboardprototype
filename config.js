89c8016d146e676857ea219216e78d041b2b0393
Chris Burgdorff <wesborland1234@aol.com>
2018-08-22 07:33:42 -0400

    Commit from VS Home

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.JWT_SECRET = "BluePanWindow732";
+config.MONGODB_CONNECT_URL = "";
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Node Modules
 var express = require('express');
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 //Instantiate Modules
 var app = express();
 //Variables
@@ -10,7 +11,33 @@ var port = process.env.PORT || 3000;
 app.get('/', function (req, res){
   res.send("HELLO THERE!");
 });  */
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public")); 
+app.get('/api', function (req, res){ //Demo route unprotected
+  res.json({
+    message: "API GET"
+  });
+});
+app.post('/api/post', function (req, res){ //demo protected route
+  res.json({
+    message: "Post created"
+  });
+});
+app.post('/api/login', function (req, res){ //demo login, use jwt stuff
+  var user = { //replace, get user from DB
+    id: 1,
+    userName: "Chris",
+    email: "wesborland"
+  };
+  jwt.sign({user: user}, 'secretkey', function (err, token){ //replace secret key with some generated key
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        token
+      });
+    }
+  });
+});
 //Start Server
 app.listen(port, function() {
   console.log("App is listening on port 3000.")
